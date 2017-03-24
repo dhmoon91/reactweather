7e24f083ba33be98f6ebc04cec0ba1b695fecd0c
dhmoon91 <dhmoon91@gmail.com>
2017-03-24 09:54:55 -0400

    different examples

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,20 +5,17 @@ var About = (props) => {
     <div>
       <h1 className="text-center page-title">About</h1>
       <p>
-        This is a weather application build on React. I have built this
-        for The Complete React Web App Developer Course.
+        This is a weather application build on React.
       </p>
       <p>
-        Here are some of the tools I used:
+      Tools:
       </p>
       <ul>
         <li>
-          <a href="https://facebook.github.io/react">React</a> - This was the
-            JavaScript framework used.
+          <a href="https://facebook.github.io/react">React</a> - Of course, react framework
         </li>
         <li>
-          <a href="http://openweathermap.org">Open Weather Map</a> - I used
-            Open Weather Map to search for weather data by city name.
+          <a href="http://openweathermap.org">Open Weather Map</a> - For weather API source
         </li>
       </ul>
     </div>
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,10 +8,10 @@ var Examples = (props) => {
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+          <Link to='/?location=Toronto'>Toronto</Link>
         </li>
         <li>
-          <Link to='/?location=Rio'>Rio, Brazil</Link>
+          <Link to='/?location=Waterloo'>Waterloo, Canada</Link>
         </li>
       </ol>
     </div>
